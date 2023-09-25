8ae768d5b732dd8873529d6aae7f816e266ab598
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-09-25 14:14:32 -0400

    Initial Commit

---
+++ MedicationManager/MedicationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicationManagerApp.swift
+//  MedicationManager
+//
+//  Created by Kevin Buchholz on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MedicationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
