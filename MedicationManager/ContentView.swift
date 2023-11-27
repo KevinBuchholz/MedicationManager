3ecc99572a2a383a030a762ca009cabcadf6b2a3
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-11-27 11:19:37 -0500

    a new start

--- MedicationManager/ContentView.swift
+++ MedicationManager/ContentView.swift
@@ -10,25 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack {
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack(alignment: .top, spacing: 10) {
-                    ForEach(1...100, id: \.self) {_ in
-                        DayView()
-                    }
-                }
-            }
-            .frame(maxWidth: 1000)
-            .edgesIgnoringSafeArea(.horizontal)
-          
-            
-            VStack {
-                RoundedRectangle(cornerRadius: 15)
-                RoundedRectangle(cornerRadius: 15)
-                RoundedRectangle(cornerRadius: 15)
-            }
-            .padding(30)
-        }
+      Text("A new start")
     }
 }
 struct ContentView_Previews: PreviewProvider {
