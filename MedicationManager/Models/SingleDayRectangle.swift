09b48d2a62a771fb87278694f3d0831185167ff4
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-11-27 11:24:10 -0500

    Added "Day view"

---
+++ MedicationManager/Models/SingleDayRectangle.swift
@@ -0,0 +1,34 @@
+//
+//  SingleDayRectangle.swift
+//  PillPopper
+//
+//  Created by Kevin Buchholz on 10/24/23.
+//
+
+import SwiftUI
+
+struct SingleDayRectangle: View {
+    var body: some View {
+        
+            RoundedRectangle(cornerRadius: 15)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundColor(.pink)
+                .opacity(0.2)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(.black, lineWidth: 1)
+                        .opacity(0.2))
+                .overlay(Text("Monday 12")
+                    .font(.largeTitle)
+                    .fontWeight(.ultraLight)
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: .infinity,
+                        alignment: .topTrailing)
+                        .padding())
+    }
+}
+
+#Preview {
+    SingleDayRectangle()
+}
